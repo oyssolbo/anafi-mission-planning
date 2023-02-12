2331fb114b66e911d904ebcc9654a52e70f765d3
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-02-12 17:02:48 +0100

    Simple sketch of running node
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning_ros2/src/move_action_node.cpp
+++ automated_planning_ros2/src/move_action_node.cpp
@@ -1,95 +1,115 @@
 #include "automated_planning_ros2/move_action_node.hpp"
-//#include "move_action_node.hpp"
 
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
 MoveAction::on_activate(const rclcpp_lifecycle::State & previous_state)
 {
-  // send_feedback(0.0, "Move starting");
+  send_feedback(0.0, "Move starting");
 
-  // navigation_action_client_ =
-  //   rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
-  //   shared_from_this(),
-  //   "navigate_to_pose");
+  // The examples use action servers and clients for controlling the operations. Due to
+  // the interaction through the ros1_bridge, actions are not supported according to 
+  // my understanding
+  // For the use with the ROS1_bridge, the method of choice should start calling the 
+  // Anafi's internal functions and monitor its progress
 
-  // bool is_action_server_ready = false;
-  // do {
-  //   RCLCPP_INFO(get_logger(), "Waiting for navigation action server...");
 
-  //   is_action_server_ready =
-  //     navigation_action_client_->wait_for_action_server(std::chrono::seconds(5));
-  // } while (!is_action_server_ready);
 
-  // RCLCPP_INFO(get_logger(), "Navigation action server ready");
+  // Check that the state is correct, such that this is not triggered once the drone has
+  // landed f.ex.
+  bool can_move = true;
+  if(! can_move)
+  {
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
+  }
 
-  // auto wp_to_navigate = get_arguments()[2];  // The goal is in the 3rd argument of the action
-  // RCLCPP_INFO(get_logger(), "Start navigation to [%s]", wp_to_navigate.c_str());
 
-  // goal_pos_ = waypoints_[wp_to_navigate];
-  // navigation_goal_.pose = goal_pos_;
+  // Publish a message to the rest of the system for desired movement, based on the current state
+  // This might be handled inside of the do_work function, assuming that it is working as intended
 
-  // dist_to_move = getDistance(goal_pos_.pose, current_pos_);
-
-  // auto send_goal_options =
-  //   rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
-
-  // send_goal_options.feedback_callback = [this](
-  //   NavigationGoalHandle::SharedPtr,
-  //   NavigationFeedback feedback) {
-  //     send_feedback(
-  //       std::min(1.0, std::max(0.0, 1.0 - (feedback->distance_remaining / dist_to_move))),
-  //       "Move running");
-  //   };
-
-  // send_goal_options.result_callback = [this](auto) {
-  //     finish(true, 1.0, "Move completed");
-  //   };
-
-  // future_navigation_goal_handle_ =
-  //   navigation_action_client_->async_send_goal(navigation_goal_, send_goal_options);
-
-  return ActionExecutorClient::on_activate(previous_state);
+  return ActionExecutorClient::on_activate(previous_state); // Will create a timer and call the do_work function?
 }
 
 
+void MoveAction::do_work()
+{
+  // For testing how the rest of the system work
+
+  // The do_work function is called at a certain time-step. This should then be used to evaluate 
+  // the progress of the system, and determine in which position it should move to based on the 
+  // current data available
+
+  static int i = 0;
+  RCLCPP_INFO(this->get_logger(), "do_work() called with i = %i", i);
+  i++;
+
+  ned_position_.point.x = 0;
+  ned_position_.point.y = 0;
+  ned_position_.point.z = 0;
+  goal_position_.point.x = 0;
+  goal_position_.point.y = 0.5;
+  goal_position_.point.z = 0;
+
+  geometry_msgs::msg::Point position = ned_position_.point; 
+  geometry_msgs::msg::Point goal = goal_position_.point;
+  double distance = std::abs(get_horizontal_distance(position, goal));
+
+  RCLCPP_INFO(this->get_logger(), "distance: %d", distance);
+
+  double radius_of_acceptance = 1.0;
+  if(distance <= radius_of_acceptance)
+  {
+    // TODO: May also want to take the vertical position into account
+    send_feedback(1.0, "Horizontal position reached");
+  }
+  else
+  {
+    send_feedback(0.0, "Testing something...");
+  }
+  finish(false, 0.99, "test");
+
+  // finish(false) will stop the plan immideately, but not alert the executor? The executor must poll the value 
+  // finish(true) starts performing the next action, until the entire plan is solved, but it does not appear to alert the executor?
+}
+
+
+
 double MoveAction::get_horizontal_distance(
-  const geometry_msgs::msg::Pose& pose1,
-  const geometry_msgs::msg::Pose& pose2)
+  const geometry_msgs::msg::Point& point1,
+  const geometry_msgs::msg::Point& point2)
 {
   return std::sqrt(
-    (pose1.position.x - pose2.position.x) * (pose1.position.x - pose2.position.x) +
-    (pose1.position.y - pose2.position.y) * (pose1.position.y - pose2.position.y)
+    std::pow((point1.x - point2.x), 2) + std::pow((point1.y - point2.y), 2) // This returns some odd values...
   );
 }
 
 
-void MoveAction::anafi_state_cb_(const std_msgs::msg::String::SharedPtr &state_msg)
+void MoveAction::anafi_state_cb_(std_msgs::msg::String::ConstSharedPtr state_msg)
 {
   std::string state = state_msg->data;
-  if(std::find(possible_anafi_states_.begin(), possible_anafi_states_.end(), state) == possible_anafi_states_.end())
-  {
-    // Not relevant information
-    return;
-  }
+  // if(std::find_if(possible_anafi_states_.begin(), possible_anafi_states_.end(), [state](std::string str){ return state.compare(str) == 0; }) == possible_anafi_states_.end())
+  // {
+  //   // No state found
+  //   return;
+  // }
   anafi_state_ = state;
 }
 
 
-void MoveAction::ekf_cb_(const anafi_uav_interfaces::msg::EkfOutput::SharedPtr &ekf_msg)
+void MoveAction::ekf_cb_(anafi_uav_interfaces::msg::EkfOutput::ConstSharedPtr ekf_msg)
 {
-
+  (void) ekf_msg;
 }
 
 
-void MoveAction::ned_pos_cb_(const geometry_msgs::msg::PointStamped::SharedPtr &ned_pos_msg)
+void MoveAction::ned_pos_cb_(geometry_msgs::msg::PointStamped::ConstSharedPtr ned_pos_msg)
 {
-
+  (void) ned_pos_msg;
 }
 
 
-void MoveAction::gnss_data_cb_(const sensor_msgs::msg::NavSatFix::SharedPtr &gnss_data_msg)
+void MoveAction::gnss_data_cb_(sensor_msgs::msg::NavSatFix::ConstSharedPtr gnss_data_msg)
 {
-
+  (void) gnss_data_msg;
 }
 
 
