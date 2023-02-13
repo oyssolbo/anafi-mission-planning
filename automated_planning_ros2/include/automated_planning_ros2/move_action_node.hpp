d809aaff437c55c3f527f471f5094582c6356424
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-02-13 11:56:06 +0100

    Added: lifecycle nodes lifecycle publishers function for moving the drone and tested it using ros1bridge other small drafts
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning_ros2/include/automated_planning_ros2/move_action_node.hpp
+++ automated_planning_ros2/include/automated_planning_ros2/move_action_node.hpp
@@ -12,6 +12,9 @@
 #include "rclcpp/subscription.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp/qos.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+#include "Eigen/Dense"
+#include "Eigen/Geometry"
 
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/pose.hpp"
@@ -19,6 +22,7 @@
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "geometry_msgs/msg/point_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/quaternion_stamped.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 
@@ -29,17 +33,23 @@
 #include "anafi_uav_interfaces/msg/ekf_output.hpp"
 
 using namespace std::chrono_literals;
+using LifecycleNodeInterface = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface;
 
 class MoveAction : public plansys2::ActionExecutorClient
 {
 public:
   MoveAction() 
-  : plansys2::ActionExecutorClient("move", 250ms)
+  : plansys2::ActionExecutorClient("move", 250ms),
+    start_distance_(1), // Initialize as non-zero
+    radius_of_acceptance_(0.75)
   {
     // Initialize the mission objectives from somewhere. This could be done during configuration tbh
     init_locations();
 
     // May have some problems with QoS when interfacing with ROS1
+    // The publishers are on mode reliable, to increase the likelihood of sending the message
+    // The subscribers are on best-effort, since similar data will be transmitted several times. If
+    // some packages are lost, so be it.
     cmd_move_by_pub_ = this->create_publisher<anafi_uav_interfaces::msg::MoveByCommand>(
       "/anafi/cmd_moveby", rclcpp::QoS(1).reliable());
     cmd_move_to_pub_ = this->create_publisher<anafi_uav_interfaces::msg::MoveToCommand>(
@@ -47,44 +57,46 @@ public:
 
     using namespace std::placeholders;
     anafi_state_sub_ = this->create_subscription<std_msgs::msg::String>(
-      "/anafi/state", rclcpp::QoS(1).reliable(), std::bind(&MoveAction::anafi_state_cb_, this, _1));   
+      "/anafi/state", rclcpp::QoS(1).best_effort(), std::bind(&MoveAction::anafi_state_cb_, this, _1));   
     ekf_output_sub_ = this->create_subscription<anafi_uav_interfaces::msg::EkfOutput>(
-      "/estimate/ekf", rclcpp::QoS(1).reliable(), std::bind(&MoveAction::ekf_cb_, this, _1));    
+      "/estimate/ekf", rclcpp::QoS(1).best_effort(), std::bind(&MoveAction::ekf_cb_, this, _1));    
     gnss_data_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(
-      "/anafi/gnss_location", rclcpp::QoS(1).reliable(), std::bind(&MoveAction::gnss_data_cb_, this, _1));
+      "/anafi/gnss_location", rclcpp::QoS(1).best_effort(), std::bind(&MoveAction::gnss_data_cb_, this, _1));
     ned_pos_sub_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
-      "/anafi/ned_pos_from_gnss", rclcpp::QoS(1).reliable(), std::bind(&MoveAction::ned_pos_cb_, this, _1));     
+      "/anafi/ned_pos_from_gnss", rclcpp::QoS(1).best_effort(), std::bind(&MoveAction::ned_pos_cb_, this, _1));     
   }
 
   // Lifecycle-events
-  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_configure(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_exit(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_error(const rclcpp_lifecycle::State &);
-  // rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &);
+  LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State &);
+  LifecycleNodeInterface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &);
+
 
 private:
   // State
+  double start_distance_;
+  double radius_of_acceptance_;
+
+  Eigen::Quaterniond attitude_{ 1, 0, 0, 0 }; // w, x, y, z
+
   std::string anafi_state_;
   anafi_uav_interfaces::msg::EkfOutput ekf_output_;
   geometry_msgs::msg::PointStamped ned_position_;
-  geometry_msgs::msg::PointStamped goal_position_;
+  geometry_msgs::msg::PointStamped goal_position_ned_;
   std::map<std::string, geometry_msgs::msg::PointStamped> locations_;
 
   const std::vector<std::string> possible_anafi_states_ = 
     {"FS_LANDED", "FS_MOTOR_RAMPING", "FS_TAKINGOFF", "FS_HOVERING", "FS_FLYING", "FS_LANDING", "FS_EMERGENCY"};
 
   // Publishers
-  rclcpp::Publisher<anafi_uav_interfaces::msg::MoveByCommand>::ConstSharedPtr cmd_move_by_pub_;
-  rclcpp::Publisher<anafi_uav_interfaces::msg::MoveToCommand>::ConstSharedPtr cmd_move_to_pub_;
+  rclcpp_lifecycle::LifecyclePublisher<anafi_uav_interfaces::msg::MoveByCommand>::SharedPtr cmd_move_by_pub_;
+  rclcpp_lifecycle::LifecyclePublisher<anafi_uav_interfaces::msg::MoveToCommand>::SharedPtr cmd_move_to_pub_;
 
   // Subscribers
   rclcpp::Subscription<std_msgs::msg::String>::ConstSharedPtr anafi_state_sub_;
   rclcpp::Subscription<anafi_uav_interfaces::msg::EkfOutput>::ConstSharedPtr ekf_output_sub_;
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::ConstSharedPtr gnss_data_sub_;
   rclcpp::Subscription<geometry_msgs::msg::PointStamped>::ConstSharedPtr ned_pos_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::QuaternionStamped>::ConstSharedPtr attitude_sub_;
 
 
   // Private functions
@@ -95,19 +107,56 @@ private:
   void init_locations();
 
   /**
-   * @brief Overload of function in ActionExecutorClient
+   * @brief Overload of function in ActionExecutorClient. This function does the 
+   * majority of the work when the node is activated. 
+   * 
+   * The function is called at the same frequency as the node, and is responsible for
+   *    - Checks whether the drone is moving
+   *      - if yes: 
+   *        - Check distance to goal and successfully terminate if reached 
+   *        - Possible update the movement vector
+   *      - if no: 
+   *        - Calculates the desired movement, and requests the drone to move
+   *        - If the drone has not moved in k number of attempts, fail the move 
+   *    - The function will publish the progress to the executor, such that the overall
+   *      system can monitor the progress
    */
   void do_work();
 
   /**
-   * @brief Calculcates the horizontal difference between two poses 
+   * @brief Calculcates the (horizontal) distance between two points
    */
-  double get_horizontal_distance(const geometry_msgs::msg::Point & pos1, const geometry_msgs::msg::Point & pos2);
+  // double get_horizontal_distance(const geometry_msgs::msg::Point & pos1, const geometry_msgs::msg::Point & pos2);
+  // double get_distance(const geometry_msgs::msg::Point & pos1, const geometry_msgs::msg::Point & pos2);
 
+  /**
+   * @brief Functions checking drone movement:
+   *  - checking whether it is capable of moving without interfering without any actions
+   *  - checking whether it is currently moving 
+   * respectively.
+   */
+  bool check_can_move();
+  bool check_movement();
+
+
+  /**
+   * @brief Definitions to publish commands easier
+   * 
+   * @warning Assumes that the publishers are already activated
+   * @warning Floats used in the moveby_cmd and not in moveto_cmd
+   */
+  void pub_moveby_cmd(float dx, float dy, float dz);
+  void pub_moveto_cmd(double lat, double lon, double h);
+
+  /**
+   * @brief Calculates the positional error
+   */
+  Eigen::Vector3d get_position_error_ned();
 
   // Callbacks
   void anafi_state_cb_(std_msgs::msg::String::ConstSharedPtr state_msg);
   void ekf_cb_(anafi_uav_interfaces::msg::EkfOutput::ConstSharedPtr ekf_msg);
   void ned_pos_cb_(geometry_msgs::msg::PointStamped::ConstSharedPtr ned_pos_msg);
   void gnss_data_cb_(sensor_msgs::msg::NavSatFix::ConstSharedPtr gnss_data_msg);
+  void attitude_cb_(geometry_msgs::msg::QuaternionStamped::ConstSharedPtr attitude_msg);
 };
--- automated_planning_ros2/src/move_action_node.cpp
+++ automated_planning_ros2/src/move_action_node.cpp
@@ -1,103 +1,226 @@
 #include "automated_planning_ros2/move_action_node.hpp"
 
-
-rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+LifecycleNodeInterface::CallbackReturn
 MoveAction::on_activate(const rclcpp_lifecycle::State & previous_state)
 {
-  send_feedback(0.0, "Move starting");
-
-  // The examples use action servers and clients for controlling the operations. Due to
-  // the interaction through the ros1_bridge, actions are not supported according to 
-  // my understanding
-  // For the use with the ROS1_bridge, the method of choice should start calling the 
-  // Anafi's internal functions and monitor its progress
-
-
-
-  // Check that the state is correct, such that this is not triggered once the drone has
-  // landed f.ex.
-  bool can_move = true;
+  // TODO: Check that the state is correct, such that this is not triggered once 
+  // the drone has landed f.ex.
+  bool can_move = check_can_move();
   if(! can_move)
   {
-    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
+    finish(false, 0.0, "Unable to start moving: Prechecks failed!");
+    return LifecycleNodeInterface::CallbackReturn::FAILURE;
   }
 
+  // Get the goal
+  const std::string goal_location = get_arguments()[2]; 
+  std::map<std::string, geometry_msgs::msg::PointStamped>::iterator it_goal_pos = locations_.find(goal_location);
+  if(it_goal_pos == locations_.end())
+  {
+    finish(false, 0.0, "Unable to find goal location!");
+    return LifecycleNodeInterface::CallbackReturn::FAILURE;
+  }
+  goal_position_ned_ = std::get<1>(*it_goal_pos);
+  start_distance_ = get_position_error_ned().norm();
 
-  // Publish a message to the rest of the system for desired movement, based on the current state
-  // This might be handled inside of the do_work function, assuming that it is working as intended
+  // Return if starting position close to the goal
+  if(start_distance_ <= radius_of_acceptance_)
+  {
+    finish(true, 1.0, "Target achieved!");
+    return LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
 
-  return ActionExecutorClient::on_activate(previous_state); // Will create a timer and call the do_work function?
+  send_feedback(0.0, "Prechecks finished. Cleared to start!");
+
+  // Activating lifecyclepublishers
+  cmd_move_by_pub_->on_activate();
+  cmd_move_to_pub_->on_activate();
+  
+  return ActionExecutorClient::on_activate(previous_state);
 }
+
+
+LifecycleNodeInterface::CallbackReturn
+MoveAction::on_deactivate(const rclcpp_lifecycle::State &)
+{
+  // Deactivate publishers
+  cmd_move_by_pub_->on_deactivate();
+  cmd_move_to_pub_->on_deactivate();
+
+  return LifecycleNodeInterface::CallbackReturn::SUCCESS;
+}
+
 
 
 void MoveAction::do_work()
 {
-  // For testing how the rest of the system work
+  // Monitoring the states between function calls
+  static bool hovering_ordered = false;
+  static int hovering_attempts = 0;
+  const int max_hovering_attempts = 10; // 2.5 seconds with 250ms rate
 
-  // The do_work function is called at a certain time-step. This should then be used to evaluate 
-  // the progress of the system, and determine in which position it should move to based on the 
-  // current data available
+  static bool move_ordered = false;
 
-  static int i = 0;
-  RCLCPP_INFO(this->get_logger(), "do_work() called with i = %i", i);
-  i++;
+  Eigen::Vector3d pos_error_ned = get_position_error_ned();
+  double distance = pos_error_ned.norm();
 
-  ned_position_.point.x = 0;
-  ned_position_.point.y = 0;
-  ned_position_.point.z = 0;
-  goal_position_.point.x = 0;
-  goal_position_.point.y = 0.5;
-  goal_position_.point.z = 0;
-
-  geometry_msgs::msg::Point position = ned_position_.point; 
-  geometry_msgs::msg::Point goal = goal_position_.point;
-  double distance = std::abs(get_horizontal_distance(position, goal));
-
-  RCLCPP_INFO(this->get_logger(), "distance: %d", distance);
-
-  double radius_of_acceptance = 1.0;
-  if(distance <= radius_of_acceptance)
+  // Check if the drone has reached the target and hovers
+  if(distance <= radius_of_acceptance_)
   {
-    // TODO: May also want to take the vertical position into account
-    send_feedback(1.0, "Horizontal position reached");
+    if(! hovering_ordered)
+    {
+      RCLCPP_INFO(this->get_logger(), "Inside sphere of acceptance for horizontal position");
+      send_feedback(0.999, "Position reached");
+      pub_moveby_cmd(0.0, 0.0, 0.0);
+      hovering_ordered = true;
+      return;
+    }
+
+    if(anafi_state_.compare("FS_HOVERING") != 0) // Check if the drone is hovering
+    {
+      // Not hovering
+      hovering_attempts++;
+      if(hovering_attempts >= max_hovering_attempts)
+      {
+        // Retry hovering
+        hovering_attempts = 0;
+        hovering_ordered = false;
+      }
+      return;
+    }
+
+    // Hovering
+    finish(true, 1.0, "Hovering close to the goal location!");
+
+    // Setting the variables to zero again for the next move
+    hovering_ordered = false;
+    hovering_attempts = 0;
+
+    move_ordered = false;
+    return;
+  }
+
+  // Not close enough to the target. Reset the variables for checking hovering
+  hovering_ordered = false;
+  hovering_attempts = 0;
+
+  // Move the drone
+  // attitude_.normalize();
+  Eigen::Vector3d pos_error_body = attitude_.toRotationMatrix().transpose() * pos_error_ned;
+
+  // std::cout << "Position error ned: " << pos_error_ned << std::endl;
+  // std::cout << "Position error body: " << pos_error_body << std::endl;
+  // std::cout << "Rotation matrix: " << attitude_.toRotationMatrix() << std::endl;
+
+  // Important to not spam the drone with new commands, as it will cancel the move
+  // https://developer.parrot.com/docs/olympe/arsdkng_ardrone3_piloting.html#olympe.messages.ardrone3.Piloting.moveBy
+
+  bool is_drone_moving = check_movement();
+  if(is_drone_moving && move_ordered)
+  {
+    // TODO: Should have some method for potentially cancel or restart the movement 
+    // if it takes too long
+    send_feedback(1.0 - distance / start_distance_, "Moving...");
+  }
+  else if(! move_ordered) 
+  {
+    float dx = static_cast<float>(pos_error_body.x());
+    float dy = static_cast<float>(pos_error_body.y());
+    float dz = static_cast<float>(pos_error_body.z());
+
+    pub_moveby_cmd(dx, dy, dz);
+    move_ordered = true;
   }
   else
   {
-    send_feedback(0.0, "Testing something...");
+    // May want a timer or counter to ensure that the drone actually moves
+    // The Anafi is quite terrible in listening at some commands...
   }
-  finish(false, 0.99, "test");
-
-  // finish(false) will stop the plan immideately, but not alert the executor? The executor must poll the value 
-  // finish(true) starts performing the next action, until the entire plan is solved, but it does not appear to alert the executor?
 }
 
 
-
-double MoveAction::get_horizontal_distance(
-  const geometry_msgs::msg::Point& point1,
-  const geometry_msgs::msg::Point& point2)
+bool MoveAction::check_can_move()
 {
-  return std::sqrt(
-    std::pow((point1.x - point2.x), 2) + std::pow((point1.y - point2.y), 2) // This returns some odd values...
-  );
+  // Currently assume that it can always move if the drone is either flying or hovering
+  return (anafi_state_.compare("FS_HOVERING") == 0) || (anafi_state_.compare("FS_FLYING") == 0);
+}
+
+
+bool MoveAction::check_movement()
+{
+  // To check movement, one could check the conditions:
+  // - the drone is flying
+  // - the position is changing (velocity comparable to the movement vector)
+  // Currently just checking that the drone is flying, such that the velocity vector 
+  // could be a future improvement
+  return anafi_state_.compare("FS_FLYING") == 0;
+}
+
+
+Eigen::Vector3d MoveAction::get_position_error_ned()
+{
+  geometry_msgs::msg::Point pos_ned = ned_position_.point;
+  geometry_msgs::msg::Point goal_pos_ned = goal_position_ned_.point;
+
+  double x_diff = pos_ned.x - goal_pos_ned.x;
+  double y_diff = pos_ned.y - goal_pos_ned.y;
+  double z_diff = pos_ned.z - goal_pos_ned.z;
+
+  Eigen::Vector3d error_ned;
+  error_ned << x_diff, y_diff, z_diff;
+  return error_ned;
+}
+
+
+void MoveAction::pub_moveby_cmd(float dx, float dy, float dz)
+{
+  anafi_uav_interfaces::msg::MoveByCommand moveby_cmd = anafi_uav_interfaces::msg::MoveByCommand();
+  moveby_cmd.header.stamp = this->now();
+  moveby_cmd.dx = dx;
+  moveby_cmd.dy = dy;
+  moveby_cmd.dz = dz;
+  moveby_cmd.dyaw = 0;
+
+  cmd_move_by_pub_->publish(moveby_cmd);
+}
+
+
+void MoveAction::pub_moveto_cmd(double lat, double lon, double h)
+{
+  anafi_uav_interfaces::msg::MoveToCommand moveto_cmd = anafi_uav_interfaces::msg::MoveToCommand();
+  moveto_cmd.header.stamp = this->now();
+  moveto_cmd.latitude = lat;
+  moveto_cmd.longitude = lon;
+  moveto_cmd.altitude = h;
+  moveto_cmd.heading = 0;
+  moveto_cmd.orientation_mode = 0; // Drone will not orient itself during movement
+  
+  cmd_move_to_pub_->publish(moveto_cmd);
 }
 
 
 void MoveAction::anafi_state_cb_(std_msgs::msg::String::ConstSharedPtr state_msg)
 {
   std::string state = state_msg->data;
-  // if(std::find_if(possible_anafi_states_.begin(), possible_anafi_states_.end(), [state](std::string str){ return state.compare(str) == 0; }) == possible_anafi_states_.end())
-  // {
-  //   // No state found
-  //   return;
-  // }
+  if(std::find_if(possible_anafi_states_.begin(), possible_anafi_states_.end(), [state](std::string str){ return state.compare(str) == 0; }) == possible_anafi_states_.end())
+  {
+    // No state found
+    return;
+  }
   anafi_state_ = state;
 }
 
 
 void MoveAction::ekf_cb_(anafi_uav_interfaces::msg::EkfOutput::ConstSharedPtr ekf_msg)
 {
-  (void) ekf_msg;
+  // Assume that the message is more recent for now... (bad assumption)
+  ekf_output_.header.stamp = ekf_msg->header.stamp;
+  ekf_output_.x_r = ekf_msg->x_r;
+  ekf_output_.y_r = ekf_msg->y_r;
+  ekf_output_.z_r = ekf_msg->z_r;
+  ekf_output_.u_r = ekf_msg->u_r;
+  ekf_output_.v_r = ekf_msg->v_r;
+  ekf_output_.w_r = ekf_msg->w_r;
 }
 
 
@@ -110,6 +233,15 @@ void MoveAction::ned_pos_cb_(geometry_msgs::msg::PointStamped::ConstSharedPtr ne
 void MoveAction::gnss_data_cb_(sensor_msgs::msg::NavSatFix::ConstSharedPtr gnss_data_msg)
 {
   (void) gnss_data_msg;
+}
+
+
+void MoveAction::attitude_cb_(geometry_msgs::msg::QuaternionStamped::ConstSharedPtr attitude_msg)
+{
+  Eigen::Vector3d v(attitude_msg->quaternion.x, attitude_msg->quaternion.y, attitude_msg->quaternion.z);
+  attitude_.w() = attitude_msg->quaternion.w;
+  attitude_.vec() = v;
+  attitude_.normalize();
 }
 
 
