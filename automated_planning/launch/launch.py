ff6a3c97f8b7e79e892694ff5781a4758d31ca18
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-02-27 08:17:14 +0100

    Rename launch_actions to launch and change pddl file to sar.pddl
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning/launch/launch.py
+++ automated_planning/launch/launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
       'launch',
       'plansys2_bringup_launch_monolithic.py')),
     launch_arguments={
-      'model_file': directory + '/pddl/move.pddl',
+      'model_file': directory + '/pddl/sar.pddl',
       'namespace': namespace
     }.items()
   )
