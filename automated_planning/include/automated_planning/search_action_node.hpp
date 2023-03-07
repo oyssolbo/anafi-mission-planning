be0c122c44515d0e06ca12a86ccbc30ed06754c2
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-03-07 08:21:22 +0100

    Add: - parameters for actions - parameters for holding locations - declare functions for locations, preconditions
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning/include/automated_planning/search_action_node.hpp
+++ automated_planning/include/automated_planning/search_action_node.hpp
@@ -45,6 +45,8 @@ public:
   SearchActionNode() 
   : plansys2::ActionExecutorClient("search_action_node", 250ms)
   , node_activated_(false)
+  , action_running_(false)
+  , search_point_idx_(0)
   {
     std::string location_prefix = "locations.";
     this->declare_parameter(location_prefix + "names", std::vector<std::string>());
@@ -62,7 +64,7 @@ public:
     search_positions_client_ = this->create_client<anafi_uav_interfaces::srv::GetSearchPositions>("/generate_search_waypoints");  
 
     // Actions
-    move_action_client_ = rclcpp_action::create_client<anafi_uav_interfaces::action::MoveToNED>(this, "/move_to_ned");
+    move_action_client_ = rclcpp_action::create_client<anafi_uav_interfaces::action::MoveToNED>(shared_from_this(), "/move_to_ned");
   }
 
   // Lifecycle-events
@@ -73,24 +75,54 @@ public:
 private:
   // State 
   bool node_activated_;
+  bool action_running_;
+
+  int search_point_idx_;
   double radius_of_acceptance_;
 
+  std::string search_location_;
   geometry_msgs::msg::Point search_center_point_;
   std::vector<geometry_msgs::msg::Point> search_points_;
+  std::map<std::string, geometry_msgs::msg::Point> ned_locations_;
 
   // Services
   rclcpp::Client<anafi_uav_interfaces::srv::GetSearchPositions>::SharedPtr search_positions_client_;
   // Service to inform the mission controller that a location is searched or not
 
   // Actions
+  using MoveGoalHandle = rclcpp_action::ClientGoalHandle<anafi_uav_interfaces::action::MoveToNED>;
+  using MoveFeedback = const std::shared_ptr<const anafi_uav_interfaces::action::MoveToNED::Feedback>;
+
   rclcpp_action::Client<anafi_uav_interfaces::action::MoveToNED>::SharedPtr move_action_client_;
+  std::shared_future<MoveGoalHandle::SharedPtr> future_move_goal_handle_;
+  MoveGoalHandle::SharedPtr move_goal_handle_;
+  anafi_uav_interfaces::action::MoveToNED::Goal move_goal_;
 
 
   // Private functions
   /**
-   * @brief Overload of function in ActionExecutorClient. This function does the 
-   * majority of the work when the node is activated
+   * @brief Initializes the location to move to, by loading from a config file and into 
+   * the map @p ned_locations_
+   */
+  void init_locations_();
+
+
+  /**
+   * @brief Overload of function in ActionExecutorClient. Nothing handled within this function 
+   * for now, as most of the actions implemented through on_activate()
    */
   void do_work();
+
+
+  /**
+   * @brief Checks whether the preconditions are satisfied for searching an area 
+   */
+  bool check_search_preconditions_();
+
+
+  /**
+   * @brief Gets a set of coordinates (NED) centered around the @p search_center_point_  
+   */
+  bool get_search_positions_();
 
 }; // SearchActionNode
