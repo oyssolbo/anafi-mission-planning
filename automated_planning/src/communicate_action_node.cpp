8b1a01887b198239f657d035fb4b5b998d95e152
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-03-08 17:31:47 +0100

    Add service for transmitting that communication finished
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning/src/communicate_action_node.cpp
+++ automated_planning/src/communicate_action_node.cpp
@@ -8,8 +8,8 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 
-
 #include "std_msgs/msg/string.hpp"
+#include "anafi_uav_interfaces/srv/set_finished_action.hpp"
 
 using namespace std::chrono_literals;
 
@@ -20,6 +20,7 @@ public:
   CommunicateActionNode()
   : plansys2::ActionExecutorClient("communicate_action_node", 500ms)
   {
+    set_finished_action_client_ = this->create_client<anafi_uav_interfaces::srv::SetFinishedAction>("/mission_controller/set_finished_action");
   }
 
 // Lifecycle-events
@@ -31,13 +32,59 @@ LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State
   RCLCPP_INFO(this->get_logger(), "Simulating communication for person " + person_id + " at location " + location); 
   finish(true, 1.0, "Communicated");
 
+  set_communicate_action_finished_();
+
   return ActionExecutorClient::on_activate(previous_state);
 }
 
 private:  
+  rclcpp::Client<anafi_uav_interfaces::srv::SetFinishedAction>::SharedPtr set_finished_action_client_;
+
   void do_work()
   {
   }
+
+bool set_communicate_action_finished_(const std::string& argument="")
+{
+  auto request = std::make_shared<anafi_uav_interfaces::srv::SetFinishedAction::Request>();
+  
+  std_msgs::msg::String action_name_msg = std_msgs::msg::String();
+  action_name_msg.data = "communicate";
+
+  std_msgs::msg::String location_msg = std_msgs::msg::String();
+  location_msg.data = get_arguments()[1]; // get_arguments returns { drone, location, person }
+
+  constexpr int num_args = 2; // Hardcoded for now
+  
+  std::vector<std_msgs::msg::String> argument_msg; 
+  
+  std_msgs::msg::String arg;
+  arg.data = get_arguments()[2];
+  argument_msg.push_back(arg);
+
+  arg.data = argument;
+  argument_msg.push_back(arg);
+
+  request->finished_action_name = action_name_msg;
+  request->location = location_msg;
+  request->arguments = argument_msg;
+  request->num_arguments = num_args;
+
+  if (! set_finished_action_client_->wait_for_service(1s)) 
+  {
+    RCLCPP_ERROR(this->get_logger(), "Service not available");
+    return false;
+  }
+
+  auto result = set_finished_action_client_->async_send_request(request);
+  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) != rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(this->get_logger(), "Unable to set action as finished!");
+    return false;
+  }
+
+  return true;
+}
 };
 
 
