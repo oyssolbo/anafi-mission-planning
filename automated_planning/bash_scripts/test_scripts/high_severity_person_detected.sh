825186f2f9180b261f0f15c0caeb7de8ae83a006
Øystein Solbø <oyssolbo@stud.ntnu.no>
2023-04-07 15:49:06 +0200

    Hardcoded value for location A0
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- automated_planning/bash_scripts/test_scripts/high_severity_person_detected.sh
+++ automated_planning/bash_scripts/test_scripts/high_severity_person_detected.sh
@@ -3,4 +3,5 @@
 source /opt/ros/foxy/setup.bash
 source ~/colcon_ws/install/setup.bash
 
-ros2 topic pub --once /estimate/person_detected anafi_uav_interfaces/msg/DetectedPerson '{id: 69, severity: 2, position: {x: 0.0, y: 0.0, z: 0.0}}'
+# Currently hardcoded in location for A0
+ros2 topic pub --once /estimate/person_detected anafi_uav_interfaces/msg/DetectedPerson '{id: 69, severity: 2, position: {x: 20.0, y: 0.0, z: 0.0}}'
